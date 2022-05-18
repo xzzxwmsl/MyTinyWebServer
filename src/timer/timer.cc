984d0225e820fdff293d9dd0ca26ffc2644e9122
xzz <1365303748@qq.com>
2022-05-18 15:35:29 +0800

    基于list的timer

---
+++ src/timer/timer.cc
@@ -0,0 +1,101 @@
+#include "timer.h"
+
+sort_timer_lst::sort_timer_lst() {}
+
+sort_timer_lst::~sort_timer_lst()
+{
+    // 删除定时器内的所有事件
+    while (!lst.empty())
+    {
+        del_timer(lst.front());
+    }
+}
+
+void sort_timer_lst::add_timer(util_timer *timer)
+{
+    // 只能增加一个副本
+    if (timer == nullptr || cache.count(timer))
+    {
+        return;
+    }
+    for (auto it = lst.begin(); it != lst.end(); it++)
+    {
+        if (timer->expire <= (*it)->expire)
+        {
+            auto t = lst.insert(it, timer);
+            cache[timer] = t;
+            return;
+        }
+    }
+    // 插入到尾巴上
+    lst.push_back(timer);
+    cache[timer] = --lst.end();
+}
+
+void sort_timer_lst::del_timer(util_timer *timer)
+{
+    if (timer == nullptr)
+    {
+        return;
+    }
+    if (cache.count(timer))
+    {
+        lst.erase(cache[timer]);
+        cache.erase(timer);
+        delete timer;
+    }
+}
+
+void sort_timer_lst::del_timer(std::list<util_timer*>::iterator it)
+{
+    util_timer* temp = *it;
+    cache.erase(temp);
+    lst.erase(it);
+    delete temp;
+}
+
+// 仅考虑超时时间延长的情况
+void sort_timer_lst::adjust_timer(util_timer *timer)
+{
+    auto nextIter = cache[timer];
+    nextIter++;
+
+    lst.erase(cache[timer]);
+    cache.erase(timer);
+
+    for (auto it = nextIter; it != lst.end(); it++)
+    {
+        if (timer->expire <= (*it)->expire)
+        {
+            auto t = lst.insert(it, timer);
+            cache[timer] = t;
+            return;
+        }
+    }
+    // 插入到尾巴上
+    lst.push_back(timer);
+    cache[timer] = --lst.end();
+}
+
+void sort_timer_lst::trick()
+{
+    if (lst.empty())
+    {
+        return;
+    }
+    auto it = lst.begin();
+    time_t cur_time = time(NULL);
+    while (it != lst.end())
+    {
+        if (cur_time < (*it)->expire)
+        {
+            break;
+        }
+        // 执行任务
+        (*it)->cb_func((*it)->user_data);
+        auto temp = it;
+        ++it;
+        // 删除资源
+        del_timer(temp);
+    }
+}
--- src/timer/timer.h
+++ src/timer/timer.h
@@ -1,8 +1,62 @@
 #ifndef TIMER_H
 #define TIMER_H
-#include<signal.h>
-struct sigaction a;
+#include <signal.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <list>
+#include <fcntl.h>
+#include<time.h>
+#include <vector>
+#include <unordered_map>
+// 前向声明
+class util_timer;
+// 用户数据结构
+struct client_data
+{
+    // 客户端socket地址
+    sockaddr_in address;
+    // socket文件描述符
+    int sockfd;
+    // 定时器
+    util_timer *timer;
+};
 
+class util_timer
+{
+public:
+    util_timer() = default;
+    util_timer(time_t exp, client_data *cd) : expire(exp), user_data(cd) {}
+    ~util_timer();
+    // 回调函数
+    void (*cb_func)(client_data *);
+    // 超时时间
+    time_t expire;
+    // 连接资源
+    client_data *user_data;
+};
 
+// 基于队列实现的定时器
+class sort_timer_lst
+{
+private:
+    // 保存定时器
+    std::list<util_timer *> lst;
+    // 保存定时器对应的迭代器位置，方便O(1)查找
+    std::unordered_map<util_timer *, std::list<util_timer *>::iterator> cache;
+
+public:
+    sort_timer_lst();
+    ~sort_timer_lst();
+    void add_timer(util_timer *);
+    void adjust_timer(util_timer *);
+    void del_timer(std::list<util_timer*>::iterator);
+    void del_timer(util_timer *);
+    void trick();
+    inline size_t getSize() const { return lst.size(); }
+};
 
 #endif
